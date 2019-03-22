816d4ac61af022d5eaef7c2713317f0cd1ed8174
云心逸 <yunxinyi+github@gmail.com>
2019-03-23 01:12:53 +0800

    Initial Commit

---
+++ Rebel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rebel
+//
+//  Created by 云心逸 on 2019/3/23.
+//  Copyright © 2019 云心逸. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
